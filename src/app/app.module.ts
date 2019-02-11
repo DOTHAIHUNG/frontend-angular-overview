36ecf1a132a44942a1aaabf836a666e589937603
HUNGDT <hk2t.thaihung@gmail.com>
2019-02-11 10:52:41 +0700

    calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FontResizeComponent } from './font-resize/font-resize.component';
 import {PetInfoComponent} from './petinfo/petinfo.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FontResizeComponent,
-    PetInfoComponent
+  AppComponent,
+  FontResizeComponent,
+  PetInfoComponent,
+  CalculatorComponent
   ],
   imports: [
-    BrowserModule
+  FormsModule,
+  BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
